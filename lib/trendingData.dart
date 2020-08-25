17e0434e239503e4b8c75b8f2e29f7d0195b2b21
mag <mahmoud.a.gaber569@gmail.com>
2020-08-25 17:05:50 +0200

    hi

---
+++ lib/trendingData.dart
@@ -0,0 +1,18 @@
+class _trend{
+
+  final String imageUrl;
+  final String text;
+
+  _trend(this.imageUrl, this.text);
+}
+
+List trend =[
+  _trend("assets/6.jpg","Bravo Leaves Manchister City"),
+  _trend("assets/7.jpg","I respect The Player,not th man David Silva'Real Sociyed move infuriates Lazio director"),
+  _trend("assets/8.jpg","Arsenal Agree 22m Deal for Coveted Lille Centre-back Gabrial Maglhaes"),
+  _trend("assets/9.jpg","Koman speaks out on 'awkward timing'of immient Barcholona move"),
+  _trend("assets/6.jpg","Arsenal Agree 22m Deal for Coveted Lille Centre-back Gabrial Maglhaes"),
+  _trend("assets/7.jpg","I respect The Player,not th man David Silva'Real Sociyed move infuriates Lazio director"),
+  _trend("assets/8.jpg","Bravo Leaves Manchister City"),
+
+];
